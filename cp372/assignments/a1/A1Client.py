7487ce53d3decbc8181f8a0cdf5c3685898af0e1
Samson Goodenough <goodenough.samson@gmail.com>
2022-01-28 10:00:41 -0500

    temp push

--- cp372/assignments/a1/A1Client.py
+++ cp372/assignments/a1/A1Client.py
@@ -1,10 +1,24 @@
+"""
+  Assignment 1: Client
+    1/26/2022 | cp372
+  
+  Group Members:
+    Riley Huston (190954880)
+    hust4880@mylaurier.ca
+    Samson Goodenough (190723380)
+    good3380@mylaurier.ca
+"""
+
 # Import socket module
+from random import random
 from socket import * 
 import sys # In order to terminate the program
 import struct
+from time import sleep
 
 # constants
 HEADER_SIZE = 8
+ENTITY = 1 # client
 
 print('\n\n\n\n\n\n\n')
 print('-'*15, 'Phase A', '-'*15)
@@ -18,44 +32,99 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(0.5)
 
 # Create a message
-entity = 1 # client
 pcode = 0 # phase 1
 data = 'Hello World!!!'
 data_length = len(data)
 
+# Send Phase A Client Packet
 format = f'!IHH{data_length}s'
-packet = struct.pack(format, data_length, pcode, entity, data.encode())
+packet = struct.pack(format, data_length, pcode, ENTITY, data.encode())
 
 clientSocket.sendto(packet, (serverName, serverPort))
+
+# Receive Phase A Server Packet
 response, serverAddress = clientSocket.recvfrom(2048)
-unpacked = struct.unpack(f'!HHIIHH', response)
-print(f'The packet received from {serverAddress} is: {unpacked}')
-serverPort = unpacked[3]
+data_length, pcode, Server_entity, repeat, udp_port, len, codeA = struct.unpack(f'!IHHIIHH', response)
+print(f'The packet received from {serverAddress} is: {(data_length, pcode, Server_entity, repeat, udp_port, len, codeA)}')
+
+# Change pcode to next phase
+pcode = codeA
 
 print('-'*15, 'Phase B', '-'*15)
-pcode = unpacked[-1]
-repeat = unpacked[2]
-len = unpacked[4]
 packet_id = 0
 
 while packet_id < repeat:
   try:
-    # send packet
+    # Send Phase B Client Packet
     data = bytearray(len + len%4)
     data_length = len + len%4
     format = f'!IHHI{data_length}s'
-    packet = struct.pack(format, data_length, pcode, entity, packet_id, data)
-
-    clientSocket.sendto(packet, (serverName, serverPort))
+    packet = struct.pack(format, data_length, pcode, ENTITY, packet_id, data)
+    print("PHASE B PCode:" ,pcode)
+    clientSocket.sendto(packet, (serverName, udp_port))
     
     # wait for response
+    # Receive Phase B-1 Server Packet
     acknowledgment, serverAddress = clientSocket.recvfrom(2048)
-    unpacked = struct.unpack(f'!IHHI', acknowledgment)
-    print(f'The packet received from {serverAddress} is: {unpacked}')
+    data_length, pcode, entity, _packet_id = struct.unpack(f'!IHHI', acknowledgment)
+    print(f'The packet received from {serverAddress} is: {(data_length, pcode, entity, _packet_id)}')
     
     packet_id += 1
-  except:
+  except Exception as e:
     print('\033[93m','='*5,'Timeout: trying again.','='*5,'\033[0m')
+    print(e)
+
+# Receive Phase B-2 Server Packet
+packet, serverAddress = clientSocket.recvfrom(2048)
+data_length, pcode, entity, tcp_port, codeB = struct.unpack('!IHHIH', packet)
+
+# Change pcode to next phase
+pcode = codeB
+
+clientSocket.close()
+
+# phase C
+print('-'*15, 'Phase C', '-'*15)
+print('Sleeping for 3 seconds...')
+
+
+# Create new TCP socket
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.settimeout(0.5)
+
+sleep(3) # wait for server to start
+
+# Connect to TCP Server Socket
+clientSocket.connect((serverName, tcp_port))
+
+# Receive Phase C Server Packet
+packet = clientSocket.recv(1024)
+data_length, pcode, server_entity, repeat2, len2, codeC, char = struct.unpack('!IHHIHHc', packet)
+print(f'The packet received from {serverAddress} is: {(data_length, pcode, server_entity, repeat2, len2, codeC, char)}')
+
+# Change pcode to next phase
+pcode = codeC
+
+# phase D
+print('-'*15, 'Phase D', '-'*15)
+data_length = 12
+pcode = codeC
+data = char
+padding = bytearray(3)
+
+for i in range(repeat2):
+  # Send Phase D Client Packet
+  packet = struct.pack(f'!IHH4s', data_length, pcode, ENTITY, data + padding)
+  clientSocket.send(packet)
+  sleep(0.5)
+
+# Receive Phase D Server Packet
+serverPacket = clientSocket.recv(2048)
+data_length, pcode, entity, codeD = struct.unpack(f'!IHHH', serverPacket)
+print(f'The packet received from {serverAddress} is: {(data_length, pcode, entity, codeD)}')
+
+# Change pcode to next phase
+pcode = codeD
 
 clientSocket.close()
 
--- cp372/assignments/a1/A1Server.py
+++ cp372/assignments/a1/A1Server.py
@@ -1,45 +1,104 @@
 # Import socket module
+from ctypes import sizeof
 import random
 import struct
 from socket import * 
 import sys
-from time import sleep # In order to terminate the program
+
+"""
+----------------------------------------------------------
+Validates the header of a unpacked packet for a UDP server
+Use: if valdiateHeaderUDP(UDPsocket, packet, clientAddress, server_pcode):
+----------------------------------------------------------
+Parameters:
+  serverSocket  - the socket in case of error
+  unpacket      - the unpacked packet
+  server_pcode  - the server pcode
+----------------------------------------------------------
+"""
+def valdiateHeaderUDP(UDPsocket, packet, clientAddress, server_pcode):
+  data_length, pcode, entity = struct.unpack('!IHH', packet[:3])
+  valid = True
+  if data_length%4 != 0:
+    valid = False
+  if pcode == server_pcode:
+    valid = False
+  if entity == 2:
+    valid = False
+  if not valid:
+    UDPsocket.sendto(-1, clientAddress)
+    UDPsocket.close()
+    sys.exit()
+
+"""
+----------------------------------------------------------
+Validates the header of a unpacked packet for a TCP server
+Use: if valdiateHeaderTCP(TCPsocket, packet, server_pcode):
+----------------------------------------------------------
+Parameters:
+  serverSocket  - the socket in case of error
+  unpacket      - the unpacked packet
+  server_pcode  - the server pcode
+----------------------------------------------------------
+"""
+def valdiateHeaderTCP(TCPsocket, packet, server_pcode):
+  data_length, pcode, entity = struct.unpack('!IHH', packet[:3])
+  valid = True
+  if data_length%4 != 0:
+    valid = False
+  if pcode == server_pcode:
+    valid = False
+  if entity == 2:
+    valid = False
+  if not valid:
+    TCPsocket.send(-1)
+    TCPsocket.close()
+    sys.exit()
+
 
 print('\n\n\n\n\n\n\n')
 print('-'*15, 'Phase A', '-'*15)
 
 # constants
 HEADER_SIZE = 8
+ENTITY = 2 # server
 
-# Assign a port number
+# assign a port number
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
-# Bind the socket to server address and server port
+# bind the socket to server address and server port
 serverSocket.bind(("", serverPort))
 
 print('The server is ready to receive')
 
+# Receive Phase A Client Packet
 packet, clientAddress = serverSocket.recvfrom(1024)
 unpacked = struct.unpack(f'!IHH{len(packet) - HEADER_SIZE}s', packet)
+pcode = 0 # phase 1
 
-# sTODO verify the packet
+# Validate packet header
+valdiateHeaderUDP(serverSocket, packet, clientAddress, pcode)
+
 print(f'The packet received from {clientAddress} is: {unpacked}')
 print(f'\tThe data is: {unpacked[-1].decode()}')
 if unpacked[-1].decode() == 'Hello World!!!':
   print('\tThe packet is correct')
 
 # response
-entity = 2 # server
-pcode = 0 # phase 1
+data_length = 16
+
 repeat = random.randint(5, 20)
 udp_port = random.randint(20000, 30000)
 len = random.randint(50, 100)
 codeA = random.randint(100, 400)
 
-# make packet
-response = struct.pack('!HHIIHH', pcode, entity, repeat, udp_port, len, codeA)
+# Send Phase A Server Packet
+response = struct.pack('!IHHIIHH', data_length, pcode, ENTITY, repeat, udp_port, len, codeA)
 serverSocket.sendto(response, clientAddress)
+
+# Change pcode to next phase
+pcode = codeA
 
 # kill the socket
 serverSocket.close()
@@ -53,24 +112,85 @@ print(f'The server is ready to receive on port {udp_port}')
 print('-'*15, 'Phase B', '-'*15)
 data_length = 4
 i = 0
+
+
 while i < repeat:
   # wait for packet
   packet, clientAddress = serverSocket.recvfrom(1024)
   if random.random() > (1/5):
     format = f'!IHHI{len+len%4}s'
+
+    # Receive Phase B Client Packet
     unpacked = struct.unpack(format, packet)
     unpacked_data = unpacked[-1].replace(b"\x00", b"0")
     print(f'Packet received from {clientAddress}\n\tpacket is: \t{unpacked[:-1]}\n\tdata is: \t0*{len+len%4}')
-    #sTODO verify the packet
     
-    # send acknowledgment
+    # Validate packet header
+    valdiateHeaderUDP(serverSocket, packet, clientAddress, pcode)
+    
+    # Send Phase B-1 Server Packet
     packet_id = unpacked[3]
     data_length = 4
-    acknowledgment = struct.pack('!IHHI', data_length, pcode, entity, packet_id)
+    acknowledgment = struct.pack('!IHHI', data_length, pcode, ENTITY, packet_id)
     serverSocket.sendto(acknowledgment, clientAddress)
+
     i += 1
+
   else:
     print('\033[93m','='*5,'Warning: Packet Dropped!','='*5,'\033[0m')
-  
 
+# Send Phase B-2 Server Packet
+data_length = 6
+tcp_port = random.randint(20000, 30000)
+codeB = random.randint(100,400)
+serverPacket = struct.pack('!IHHIH', data_length, pcode, ENTITY, tcp_port, codeB)
+serverSocket.sendto(serverPacket, clientAddress)
+serverSocket.close()
+
+# Change pcode to next phase
+pcode = codeB
+
+# phase C
+print('-'*15, 'Phase C', '-'*15)
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("", tcp_port))
+serverSocket.listen(5)
+connectionSocket, addr = serverSocket.accept()
+print ('The server is ready to receive')
+
+data_length = 15 # IHHIHc
+pcode = codeB
+repeat2 = random.randint(5, 20)
+len2 = random.randint(50, 100)
+codeC = random.randint(100, 400)
+char = chr(random.randint(65, 90))
+# Send Phase C Server Packet
+serverPacket = struct.pack('!IHHIHHc', data_length, pcode, ENTITY, repeat2, len2, codeC, char.encode())
+connectionSocket.send(serverPacket)
+
+# Change pcode to next phase
+pcode = codeC
+
+# phase D
+print('-'*15, 'Phase D', '-'*15)
+
+# Receive Phase D Client Packet
+
+for i in range(repeat2):
+  packet = connectionSocket.recv(2048)
+  unpacked = struct.unpack('!IHH4s', packet)
+
+  # Validate packet header
+  valdiateHeaderTCP(serverSocket, packet, pcode)
+
+  print(unpacked)
+
+
+data_length = 15 # IHHIHc
+pcode = codeC
+codeD = random.randint(100, 400)
+serverPacket = struct.pack('!IHHH', data_length, pcode, ENTITY, codeD)
+connectionSocket.send(serverPacket)
 sys.exit() # Terminate the program after sending the corresponding data
+
